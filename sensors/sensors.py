5ff3fcb9984ce29ca75b3bfae01f6e84d826f863
brettlangdon <brett.langdon@datadoghq.com>
2019-07-11 12:19:46 -0400

    fix up sensors app

--- sensors/sensors.py
+++ sensors/sensors.py
@@ -1,23 +1,25 @@
-import requests
-
-from flask import Flask, Response, jsonify
-from flask import request as flask_request
-
-from ddtrace import tracer
-
-from bootstrap import create_app, db
-from models import Network, Sensor
-
+# stdlib
 import random
 import time
+
+# 3rd party
+from ddtrace import tracer
+from flask import Response, jsonify
+from flask import request as flask_request
+
+# internal
+from bootstrap import create_app, db
+from models import Sensor
 
 sensors = []
 
 app = create_app()
 
+
 @app.route('/')
 def hello():
     return Response({'Hello from Sensors': 'world'}, mimetype='application/json')
+
 
 @app.route('/sensors', methods=['GET', 'POST'])
 def get_sensors():
@@ -32,24 +34,28 @@ def get_sensors():
                         'system_status': system_status})
     elif flask_request.method == 'POST':
         sensors.append({'sensor_no': len(sensors) + 1, 'value': random.randint(1,100)})
-        app.logger.info(f"Sensor number {len(sensors)} created")
+        app.logger.info(f'Sensor number {len(sensors)} created')
         return jsonify(sensors)
     else:
         err = jsonify({'error': 'Invalid request method'})
         err.status_code = 405
         return err
 
+
 @app.route('/sensors/<id>/')
 def sensor(id):
-    app.logger.info(f"Getting info for sensor {id}")
+    app.logger.info(f'Getting info for sensor {id}')
     return jsonify(Sensor.query.get(id).serialize())
+
 
 @app.route('/refresh_sensors')
 def refresh_sensors():
-    app.logger.info("Calling refresh sensor simulator")
+    app.logger.info('Calling refresh sensor simulator')
     sensors = simulate_all_sensors()
+
     return jsonify({'sensor_count': len(sensors),
                     'system_status': sensors})
+
 
 @tracer.wrap(name='sensor-simulator')
 def simulate_all_sensors():
@@ -58,5 +64,5 @@ def simulate_all_sensors():
         sensor.value = random.randint(1,100)
     db.session.add_all(sensors)
     db.session.commit()
-    app.logger.info("Sensor data updated")
+    app.logger.info('Sensor data updated')
     return [s.serialize() for s in sensors]
